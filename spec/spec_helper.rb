eee3092bb297236fe3aebbfbdfa8d72c4bbf4fe4
Dawn Cronin <dawncronin7@gmail.com>
2019-10-10 23:37:21 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,22 @@ RSpec.configure do |config|
     return Hash.new
     # raise "replace underscore with code"
   end
-
+ def my_hash
+   return {:name => "Dawn"}
+ end
+ 
+ def pioneer
+   return {:name => 'Grace Hopper'}
+ end
+ 
+ def id_generator
+   return {:id => 2389028}
+ end
+ 
+ def my_hash_creator(key,value)
+   return {:key => value}
+ end
+ 
+ def read_from_hash(hash,key)
+   
 end
